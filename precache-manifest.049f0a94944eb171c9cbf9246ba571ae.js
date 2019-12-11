cdfcb85c317d024f21209f6ba5e53d5928adfcf6
Preston Chandler <pchandler858@gmail.com>
2019-12-11 15:00:48 -0800

    Updates

---
+++ precache-manifest.049f0a94944eb171c9cbf9246ba571ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2611d2928d6a787a01e4e11cba793034",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03135ed9a32f1fb8e4c6",
+    "url": "/static/css/main.f2929533.chunk.css"
+  },
+  {
+    "revision": "da4ece4c3616c900c3f2",
+    "url": "/static/js/2.a865379f.chunk.js"
+  },
+  {
+    "revision": "03135ed9a32f1fb8e4c6",
+    "url": "/static/js/main.015f9c24.chunk.js"
+  },
+  {
+    "revision": "46ad358fbf11f685beb8",
+    "url": "/static/js/runtime-main.b286bdf5.js"
+  }
+]);
